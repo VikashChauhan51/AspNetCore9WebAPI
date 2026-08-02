eebf297569c36d2d4ca4c81c706461eba94f5b06
Vikash Chauhan <vikashchauhan51@gmail.com>
2026-08-02 17:16:09 +0530

    added activity sources

---
+++ src/Core/CourseLibrary.Infrastructure/Observability/Logging/ILogEnricher.cs
@@ -0,0 +1,11 @@
+﻿using OpenTelemetry.Logs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseLibrary.Infrastructure.Observability.Logging;
+
+public interface ILogEnricher
+{
+    void Enrich(LogRecord record);
+}
--- src/Core/CourseLibrary.Infrastructure/Observability/Tracing/IActivityEnricher.cs
+++ src/Core/CourseLibrary.Infrastructure/Observability/Tracing/IActivityEnricher.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 
-namespace CourseLibrary.Infrastructure.Observability.Enrichers;
+namespace CourseLibrary.Infrastructure.Observability.Tracing;
 
 public interface IActivityEnricher
 {
